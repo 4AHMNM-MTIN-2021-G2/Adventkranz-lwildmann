c2eb7bfc28301e72ce64729a624a4aad364770b5
lwildmann <wildmannlukas03@gmail.com>
2020-12-15 21:47:35 +0100

    finished System.DateTime Script

---
+++ Assets/MyGame/Scripts/CountDays.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDays : MonoBehaviour
+{
+    System.DateTime christmasDay;
+    System.DateTime firstAdvent;
+    System.DateTime secondAdvent;
+    System.DateTime thirdAdvent;
+    System.DateTime fourthAdvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        firstAdvent = new System.DateTime(2020, 11, 29);
+        secondAdvent = new System.DateTime(2020, 12, 06);
+        thirdAdvent = new System.DateTime(2020, 12, 13);
+        fourthAdvent = new System.DateTime(2020, 12, 20);
+        christmasDay = new System.DateTime(2020, 12, 24);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
